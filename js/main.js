6403602cb219b37cd3173b3142883b3bfaed3099
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-03 02:30:46 +0300

    Assignment Compeleted

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let imgsNumber = 6;
+let idx = 0;
+const galleryContainer = document.querySelector("main section .container .row");
+const caruosel = document.querySelector("main section .carousel");
+const caruoselImage = document.querySelector("main section .carousel img");
+galleryContainer.innerHTML = "";
+for (var i = 0; i < imgsNumber; i++) {
+  galleryContainer.innerHTML += `<div class="card-container col-xl-4  col-md-6 col-12">
+    <div class="my-card">
+      <div class="img-container mb-3">
+        <button class="d-block w-100 h-100">
+        <img data-idx="${i}" src="imgs/${i + 1}.png" alt="" />
+                </button>
+      </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+      voluptate!
+    </p>
+  </div>`;
+}
+
+function toggleCaruosel() {
+  caruosel.style.display = caruosel.style.display === "none" ? "" : "none";
+}
+
+function setImage(i) {
+  i = Number(i);
+  caruoselImage.setAttribute("src", `imgs/${i + 1}.png`);
+  idx = i;
+}
+
+function changeIdx(inc){
+  idx = (idx + inc + imgsNumber) % imgsNumber;
+  console.log(idx)
+  setImage(idx);
+}
+
+galleryContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    toggleCaruosel();
+    setImage(e.target.dataset.idx);
+  }
+});
+
+caruosel.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.tagName === "DIV" || e.target.classList.contains("close"))
+    toggleCaruosel();
+  else if (e.target.classList.contains("left")) {
+    changeIdx(1);
+  } else if (e.target.classList.contains("right")) {
+    changeIdx(-1);
+  }
+});
